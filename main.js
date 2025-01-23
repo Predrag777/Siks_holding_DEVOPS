d2a4d5b536b6f71f1a000b033f7a4e579fdee12a
Predrag777 <predragbrajovicSS@gmail.com>
2025-01-23 14:05:22 +0100

    SS

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-/*require('dotenv').config(); // load environment variables from .env file
+//require('dotenv').config(); // load environment variables from .env file
 const { error } = require("console");
 const express=require("express")
 const mysql=require("mysql")
+const cors=require('cors')
 
-const port = process.env.PORT || 3000;
+const port = 3307;
+
+
+var whitelist=["::ffff:127.0.0.12"]
 
 
 const app=express()
@@ -22,30 +26,45 @@ var conn = mysql.createConnection({
     password: "",
     database: 'X'
 });
+var log=false
 try{
     conn.connect()
     app.get('/', (req, res)=>{
+        for(let i in whitelist){
+            if(whitelist[i]==req.ip){
+                log=true
+            }
+        }
+        if(!log){
+            console.log("You are blocked")
+        }else{
+            console.log("Prosao")
+        }
         res.render("index.html")
-
-        conn.query("SELECT IP FROM visitors", (err, rows, fields)=>{
+        var found=false
+       conn.query("SELECT IP FROM visitors", (err, rows, fields)=>{
             if(err) throw err
             for(let i=0;i<rows.length;i++){
                 
                 if(rows[i].IP==req.ip){
-                    conn.query(`UPDATE visitors SET num_of_visiting=num_of_visiting+1 WHERE IP='${req.ip}'`, (err)=>{
-                        if(err) throw err
-                        console.log(rows[i].IP+"    "+req.ip+"   "+(rows[i].IP==req.ip))
-                        console.log("Postoji, pa povecaj brojac posjeta")
-                    })
+                    found=true
                     
-                }else{
-                    conn.query(`INSERT INTO visitors VALUES(NULL, '${req.ip}', 1)`, (err)=>{
-                        if(err) throw err
-                        console.log("Dodata ip addresa")
-                    })
                 }
             }
         })
+
+        if(found){
+            conn.query(`UPDATE visitors SET num_of_visiting=num_of_visiting+1 WHERE IP='${req.ip}'`, (err)=>{
+                if(err) throw err
+                console.log(rows[i].IP+"    "+req.ip+"   "+(rows[i].IP==req.ip))
+                console.log("Postoji, pa povecaj brojac posjeta")
+            })
+        }else{
+            conn.query(`INSERT INTO visitors VALUES(NULL, '${req.ip}', 1)`, (err)=>{
+                if(err) throw err
+                console.log("Dodata ip addresa")
+            })
+        }
 
 
 
@@ -80,22 +99,9 @@ try{
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-*/
+
 
 
 /////Kopirani kod:
 
-require('dotenv').config(); // load environment variables from .env file
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; // default port is 3000
 
-app.get('/', (req, res) => {
-    return res.status(200).json({
-        message: 'Hello World'
-    });
-});
-
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
